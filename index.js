6f0f88ab6677352631c77646932204ecd64a36aa
amandakms <amandakessila@gmail.com>
2020-03-12 08:09:30 -0300

    corrigindo

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ console.log("Hello Word");
 //Arvore de Natal
 const num_linhas = 10;
 
-for(let i =0; i < num_linhas,i++) {
-    let espacos = " ".repeat(num-linhas - i)
+for(let i =0, i < num_linhas, i++) {
+    let espacos = " ".repeat(num_linhas - i)
     let conteudo = 'x'.repeat(i);
     console.log(espaco + conteudo +'x' + conteudo);
 }
 
-console.log((' '.repeat(num_linhas)+ 'x' + '\n'.repeat(4));
+console.log(' '.repeat(num_linhas)+ 'x' + '\n'.repeat(4));
