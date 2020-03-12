65c612a54411f60c6ebc8410a3b5565b726b9fc2
amandakms <amandakessila@gmail.com>
2020-03-12 08:19:07 -0300

    corrigindo2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ console.log("Hello Word");
 const num_linhas = 10;
 
 for(let i = 0; i < num_linhas; i++) {
-    let espacos = " ".repeat(num_linhas - i)
+    let espaco = " ".repeat(num_linhas - i)
     let conteudo = 'x'.repeat(i);
     console.log(espaco + conteudo +'x' + conteudo);
 }
